fc3adaf1f0d6a2585f567812df681b7ed2cca41b
wnstjr541 <wnstjr541@naver.com>
2022-08-24 21:20:19 +0900

    검색시 마커 추가

--- src/pages/recommendation/kakaoMap/KakaoMap.js
+++ src/pages/recommendation/kakaoMap/KakaoMap.js
@@ -25,6 +25,7 @@ export default function kakaoMap(searchType ,setSearchType , setKakaoStreetAddre
 
     var marker = new kakao.maps.Marker() // 클릭한 위치를 표시할 마커입니다
 
+
     // 지도를 클릭했을 때 클릭 위치 좌표에 대한 주소정보를 표시하도록 이벤트를 등록합니다
     kakao.maps.event.addListener(map, 'click', function(mouseEvent) {
         searchDetailAddrFromCoords(mouseEvent.latLng, function(result, status) {
@@ -43,23 +44,49 @@ export default function kakaoMap(searchType ,setSearchType , setKakaoStreetAddre
         });
     });
     if(keyword !== '' ){
-        kakaoMapPanTo(map, Number(searchData?.data[0]?.y) ,  Number(searchData?.data[0]?.x) );
+        kakaoMapPanTo(map, searchData?.data?.length  ? Number(searchData?.data[0]?.y) :  Number(searchData?.data?.y) , searchData?.data?.length ? Number(searchData?.data[0]?.x) :  Number(searchData?.data?.x));
     }
+
+    if(searchData?.data?.length ){    
+        for (var i=0; i<searchData?.data?.length; i++) {
+            displayMarker(searchData?.data[i] , map , keyword);    
+        }   
+    }else{
+        displayMarker(searchData?.data , map , keyword);
+    }    
 }
 
-// 마커를 생성하고 지도위에 표시하는 함수입니다
-function addMarker(position , map) {
-    
-    // 마커를 생성합니다
-    var marker = new kakao.maps.Marker({
-        position: [position]
+// 지도에 마커를 표시하는 함수입니다
+function displayMarker(place,  map , keyword) {
+
+    var iwContent = place?.place_name, // 인포윈도우에 표시할 내용
+        iwRemoveable = true;
+
+    // 인포윈도우를 생성합니다
+    var infowindow = new kakao.maps.InfoWindow({
+        content: iwContent,
+        removable: iwRemoveable
     });
 
-    // 마커가 지도 위에 표시되도록 설정합니다
-    marker.setMap(map);
-    
-    // 생성된 마커를 배열에 추가합니다
-    // markers.push(marker);
+
+    // 마커를 생성하고 지도에 표시합니다
+    var markers = new kakao.maps.Marker({
+        map: map,
+        position: new kakao.maps.LatLng(place?.y , place?.x)
+    });
+    if(keyword !== ""){
+        markers.setMap(map)
+    }else {
+        markers.setMap(null)
+    }
+
+    // 마커에 클릭이벤트를 등록합니다
+    kakao.maps.event.addListener(markers, 'click', function () {
+        // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
+        infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place?.place_name + '</div>');
+        infowindow.open(map, markers);
+    });
+
 }
 
 function searchDetailAddrFromCoords(coords, callback) {
